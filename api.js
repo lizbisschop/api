b3777f9b1320156e61e25a5864d5d74dd60e3fef
Liz Bisschop <lbisschop@livit.nl>
2024-04-11 22:43:37 +0200

    testing change

--- api.js
+++ api.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Define a route for GET requests to '/api/hello'
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello, World!' });
+    res.json({ message: 'Hello, World! what are you upto huh?' });
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
