bfc048aa3abb56599be84c68b002359f7fe0b34d
Liz Bisschop <lbisschop@livit.nl>
2024-04-12 11:20:58 +0200

    poging 2

--- api.js
+++ api.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Define a route for GET requests to '/api/hello'
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello, World! niewe dag niewe kansen' });
+    res.json({ message: 'Hello, World! dit is pogin twee om te pullen en te starten hihi' });
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = 2000;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
