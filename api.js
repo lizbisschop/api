0f82657482e9100070d830a8aa0100f649e43596
Liz Bisschop <lbisschop@livit.nl>
2024-04-12 19:01:58 +0200

    done

--- api.js
+++ api.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Define a route for GET requests to '/api/hello'
 app.get('/', (req, res) => {
-    res.json({ "result": 'hello world toch nog even testen' });
+    res.json({ "result": 'hello world' });
 });
 
 // Start the server
